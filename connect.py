4f814c3d8fdf5a36e40039e7c837e9bb168f099e
s0nelywy111 <156456750+s0nelywy111@users.noreply.github.com>
2025-03-25 14:10:21 +0100

    Connect.py

--- connect.py
+++ connect.py
@@ -6,10 +6,5 @@ client1.connect(('192.168.137.57', 1111))
 flag = True
 while flag:
     client1.send(input("Me: ").encode('utf-8'))
-    msg = client1.recv(2048).decode('utf-8')
-    if msg == 'exit':
-        flag = False
-    else:
-        print(msg)
 client1.close()
 
